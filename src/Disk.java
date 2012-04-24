11e5c63e6bb463cf2ae327116fb1c39c52482b5a
unknown <milba894@CYD48.(none)>
2012-04-24 15:14:04 +0200

    Nu syns bilden i spelrutan

--- src/Disk.java
+++ src/Disk.java
@@ -1,5 +1,6 @@
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -7,18 +8,19 @@ import javax.imageio.ImageIO;
 public class Disk
 {
 	private Image diskImage;
-	private double x=500;
-	private double y=500;
+	private double x=0.1;
+	private double y=800;
 	
 	
-	public Disk()
+	public Disk(GameCanvas parent)
 	{	
 		try {
 			diskImage = ImageIO.read(getClass().getResource("testbild.jpg"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+	
+		diskImage = parent.makeColorTransparent((BufferedImage) diskImage);
 	}
 	
 	
