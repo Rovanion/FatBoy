fbaeeaf72931431ee86bf500370e81720d15b4fb
unknown <Robert@Robert-Dator.(none)>
2012-04-25 21:46:27 +0200

    Lade TitleScreen i egen klass.

---
+++ src/TitleScreen.java
@@ -0,0 +1,39 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+
+public class TitleScreen 
+{
+	private Image titleScreen;
+	private boolean showTitleScreen;
+	
+	public TitleScreen(Image img)
+	{
+		titleScreen = img;
+		showTitleScreen=true;
+		
+	}
+	
+	public boolean isShowTitleScreen() {
+		return showTitleScreen;
+	}
+
+	public void setShowTitleScreen(boolean showTitleScreen) {
+		this.showTitleScreen = showTitleScreen;
+	}
+
+	public void update(Controller controller)
+	{
+		if (controller.keys[KeyEvent.VK_ENTER]) 
+		{
+			showTitleScreen = false;
+		}
+	}
+	
+	public void render(Graphics2D g, int width, int height)
+	{
+		g.drawImage(titleScreen, 0, 0, width, height, null);
+	}
+
+}
