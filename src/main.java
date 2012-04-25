6c4cf8af6c3283d48a2c93f6792bd8fdfca150b8
Jonas Skog <Jonsk197@student.liu.se>
2012-04-25 12:19:41 +0200

    har börjat med att öppna/spara fil.

--- src/main.java
+++ src/main.java
@@ -1,3 +1,10 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 public class main {
 
 	/**
@@ -9,10 +16,34 @@ public class main {
 		GameCanvas newCanvas = new GameCanvas();
 		CreateFrame frame = new CreateFrame(newCanvas);
 	}
+
 	public static void endGame(){
-		//Load the hiscore from file and show it
+		HighScoreList list = null;
+		
+		try {
+			// deserialize from filen
+			ObjectInputStream in =new ObjectInputStream(new FileInputStream("list.ser"));
+			in.close();
+			
+		}catch(IOException e){
+			HighScoreList HighScoreList = new HighScoreList();
+			
+		}
+		
+		
+		
+		//Visar listan samt sparar namn.
+		
+		try {	
+			//Seralize to fil
+			ObjectOutput out = new ObjectOutputStream(new FileOutputStream("list.ser"));
+	    out.writeObject(list);
+	    out.close();
+	    
+		}
+		catch(IOException e){
+		}
 		
 		System.exit(0);
 	}
-
 }
