b0c295f4d43eea6b5e3c1528a6f122649a0c5db5
Rovanion Luckey <rovanion.luckey@gmail.com>
2012-04-26 12:29:09 +0200

    Ändringar för den nya bilden och ändringar för annorlunda fysik.

--- src/FatBoyHero.java
+++ src/FatBoyHero.java
@@ -3,137 +3,132 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 
-public class FatBoyHero 
-{
-	//FIELDS
+public class FatBoyHero {
+	// FIELDS
 	private Image image;
-	private double x = 0.5;
-	private double y = 0.7;
+	private double x = 0.3;
+	private double y = 0.8;
 	private double dx = 0;
 	private double dy = 0;
-	private double fatLevel = 1.6; //Max:1.7 , Min:0.82 (Med dessa slipper änden av repet synas vid något av tillfällen fet-smal.
+	private double fatLevel = 1.6; // Max:1.7 , Min:0.82 (Med dessa slipper
+									// ï¿½nden av repet synas vid nï¿½got av
+									// tillfï¿½llen fet-smal.
 	private double counterLimit = 0.05;
-	private double groundLevel = 0.7;
-	private int jump = 0;
+	private double groundLevel = 0.8;
+	private int jumpKeyPresses = 0;
+	private boolean jumping = false;
 
-	//CONSTRUCTOR
+	// CONSTRUCTOR
 	public FatBoyHero(Image image) {
 		this.image = image;
 	}
 
-	//METHODS
+	// METHODS
 	/**
 	 * Update keeps track on FatBoy's movement corresponding to the controller.
+	 * 
 	 * @param controller
 	 */
-	public void update(Controller controller) 
-	{
+	public void update(Controller controller) {
 		// Movement X-axis
 		dx = dx * (1 - 0.1 * fatLevel);
-		
 
-		dx += 0.00005*(1.5-x)*(0.8*fatLevel); //Elastic rope: ForcePull=k*xChange*percentageOfMass
-		if(fatLevel<0.82)
-		{
-			dx+=0.0003; //If too thin you get dragged out.
+		dx += 0.00005 * (1.5 - x) * (0.8 * fatLevel); // Elastic rope:
+														// ForcePull=k*xChange*percentageOfMass
+		if (fatLevel < 0.82) {
+			dx += 0.0003; // If too thin you get dragged out.
+		} 
+		else if (fatLevel > 1.5) {
+			dx = dx / 4;
 		}
-		else if(fatLevel>1.5)
-		{
-			dx=dx/4;
-		}
-		//dx += 0.004 - (0.004 * fatLevel);
+		// dx += 0.004 - (0.004 * fatLevel);
 
-		if (controller.keys[KeyEvent.VK_RIGHT]|| controller.keys[KeyEvent.VK_D]) //MOVE RIGHT
-		{
+		if (controller.keys[KeyEvent.VK_RIGHT] || controller.keys[KeyEvent.VK_D]){
 			dx += 0.0003 * fatLevel;
 		}
-		if (controller.keys[KeyEvent.VK_LEFT] || controller.keys[KeyEvent.VK_A]) //MOVE LEFT
-		{
+		if (controller.keys[KeyEvent.VK_LEFT] || controller.keys[KeyEvent.VK_A]){
 			dx -= 0.00015 * fatLevel;
-			fatLevel -= 0.0003; //Moving against the rope's force burns fatpoints.
+			//Moving against the rope force burns fat
+			fatLevel -= 0.0003;
 		}
-		
+
 		x += dx;
-		
+
 		limitScreenMovement();
+		
 
 		// Movement Y-axis
-		if (controller.keys[KeyEvent.VK_UP] || controller.keys[KeyEvent.VK_W]) 
-		{
+		if (controller.keys[KeyEvent.VK_UP] || controller.keys[KeyEvent.VK_W]) {
 			jump();
 		}
-		
-		y += dy;
+		/*
+		 * This prevents double-jumping.
+		 * By setting the number of jump-key presses over the allowed value 
+		 * for adding more speed, no more force will be added if the key is 
+		 * pressed again.
+		 */
+		else if(jumping){
+			jumpKeyPresses = 100;
+		}
 
-		resetGroundLevel();	
-	}
-	
-	/**SCREEN LIMITATION
-	 * Makes the player stop if they reach the counter at the left side of
-	 * the map and terminates the game if they go outside of the screen to
-	 * the right.
-	 */
-	private void limitScreenMovement()
-	{
-		if (x < counterLimit)
-		{
-			x = counterLimit;
-			dx = 0;
-		} 
-		else if (x >= 0.9)
-		{
-			main.endGame();
-		}
-	}
-	
-	/**
-	 * JUMP
-	 * Changes the y-ccordinates.
-	 */
-	private void jump()
-	{
-		jump++;
-		if (jump < 12) {
-			if (jump == 1)
-				dy = -0.005 * (1 - 0.25 * fatLevel);
-			dy -= 0.004 * (1 - 0.25 * fatLevel);
-		}
-	}
-	
-	/**RESET GROUND LEVEL
-	 * If the player has jumped; Once he reaches the ground level again
-	 * reset his speed and y-position.
-	 */
-	private void resetGroundLevel()
-	{
-		if (jump > 0) 
-		{
+		y += dy;
+		
+		//If the player is jumping, add the gravitational acceleration to dy.
+		if (jumpKeyPresses > 0) {
 			dy += 0.0009;
-			if (y > groundLevel) 
-			{
+			
+			//If the player just hit the ground, reset speed and position.
+			if (y > groundLevel) {
 				dy = 0;
 				y = groundLevel;
-				jump = 0;
+				jumpKeyPresses = 0;
+				//Jumping burns fat.
+				fatLevel -= 0.02;
 			}
 		}
 	}
+
+	/**
+	 * SCREEN LIMITATION Makes the player stop if they reach the counter at the
+	 * left side of the map and terminates the game if they go outside of the
+	 * screen to the right.
+	 */
+	private void limitScreenMovement() {
+		if (x < counterLimit) {
+			x = counterLimit;
+			dx = 0;
+		} else if (x >= 0.9) {
+			main.endGame();
+		}
+	}
+
+	/**
+	 * JUMP Changes the y-ccordinates.
+	 */
+	private void jump() {
+		jumpKeyPresses++;
+		if (jumpKeyPresses < 12) {
+			if (jumpKeyPresses == 1)
+				dy = -0.02 * (1 - 0.35 * fatLevel);
+			dy -= 0.003 * (1 - 0.35 * fatLevel);
+		}
+	}
+
 
 	public void render(Graphics2D g) {
 
 		int absoluteX = (int) (x * GameCanvas.width());
 		int absoluteY = (int) (y * GameCanvas.height());
 
-		// Set the coordinate system relative to the fat boy
+		// Set origaa to the upper left corner of the boy
 		g.translate(absoluteX, absoluteY);
-		
+
 		g.setColor(Color.ORANGE);
-		g.fillRect((int) (0.14 * GameCanvas.width()),
+		g.fillRect((int) (0.1 * GameCanvas.width()),
 				(int) (0.20 * GameCanvas.height()), 2000, 6);
 
-		g.drawImage(image, 0, 0, (int) (300 * fatLevel),
-				(int) (300 /*(1 - 0.2 * fatLevel)*/), null);
-
-		
+		g.drawImage(image, 0, 0, (int) (GameCanvas.width() * 0.1 * fatLevel), 
+						(int) (GameCanvas.height() * 0.15), null);
 
 		// Reset the coordinate system to the top left of the screen
 		g.translate(-absoluteX, -absoluteY);
