a3ccd0cf30f6d864c791da482c76ee36a8371a58
Jonas Skog <Jonsk197@student.liu.se>
2012-05-01 16:34:18 +0200

    Har skapat ett fönster för att ange namn.

--- src/main.java
+++ src/main.java
@@ -1,9 +1,17 @@
+import java.awt.Button;
+import java.awt.Container;
+import java.awt.TextField;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class main {
 
@@ -17,33 +25,67 @@ public class main {
 		CreateFrame frame = new CreateFrame(newCanvas);
 	}
 
-	public static void endGame(){
-		HighScoreList list = null;
+	public static void endGame() {
+		HighscoreManager hm = new HighscoreManager();
+		String inputName = null;
+		int points = 0;
+
+		// Här skapas det ett förnster för att ta in namn och lägga till det
+		// till poängen"
+	
+		hm.addScore(inputName, points);
+
+			//
+			// Skapa ett fönster.
+			//
+			JFrame fönster = new JFrame();
+			fönster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			fönster.setSize(200, 150); // Bredd och höjd.
+			fönster.setLocation(100, 100); // x och y.
+			fönster.setTitle("Ange ditt namn");
+
+			//
+			// Skapa knappen och textrutan.
+			//
+			JButton knapp = new JButton("Klar");
+			JTextArea textruta = new JTextArea();
+
+			//
+			// Man lägger komponenterna i en
+			// "content pane" (innehållspanel).
+			//
+			Container innehåll = fönster.getContentPane();
+
+			//
+			// Här talar vi om att vi inte vill ha någon
+			// automatisk layouthantering av komponenterna
+			// i fönstret (null = ingenting/tomt).
+			//
+			innehåll.setLayout(null);
+
+			//
+			// Nu lägger vi in komponenterna och positionerar dem.
+			// Koordinatsystemet börjar i övre vänstra hörnet,
+			// y växer nedåt. Enheten är bildpunkter.
+			//
+			innehåll.add(knapp);
+			innehåll.add(textruta);
+
+			//
+			// Argumenten till setBounds() är x, y, bredd, höjd.
+			//
+			textruta.setBounds(10, 10,180 ,20);
+			knapp.setBounds(65, 40, 70, 40);
+
+			//
+			// Öppna fönstret.
+			//
+			fönster.show();
 		
-		try {
-			// deserialize from filen
-			ObjectInputStream in =new ObjectInputStream(new FileInputStream("list.ser"));
-			in.close();
-			
-		}catch(IOException e){
-			HighScoreList HighScoreList = new HighScoreList();
-			
-		}
 		
+		System.out.print(hm.getHighscoreString());
 		
-		
-		//Visar listan samt sparar namn.
-		
-		try {	
-			//Seralize to fil
-			ObjectOutput out = new ObjectOutputStream(new FileOutputStream("list.ser"));
-	    out.writeObject(list);
-	    out.close();
-	    
-		}
-		catch(IOException e){
-		}
-		
-		System.exit(0);
+//System.exit(0);
+
 	}
 }
