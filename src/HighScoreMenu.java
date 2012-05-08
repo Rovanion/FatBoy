c5696e0c3c8a0f43650941be4051b1d5e30522df
Jonas Skog <Jonsk197@student.liu.se>
2012-05-08 19:57:15 +0200

    Klassen tar nu in namn och sparar det som en variabel. Den ger även ett nytt fönster för att ange namn.

--- src/HighScoreMenu.java
+++ src/HighScoreMenu.java
@@ -2,55 +2,78 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class HighScoreMenu
-{
-    public static void inputName()
-    {
-        //
-        // Skapa ett fönster.
-        //
-        JFrame window= new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(300, 400);  // Bredd och höjd.
-        window.setLocation(100, 100);  // x och y.
-        window.setTitle("Fönster med komponenter");
+public class HighScoreMenu {
 
-        //
-        // Skapa knappen och textrutan.
-        //
-        JButton knapp = new JButton("Klicka mej");
-        JTextArea textruta = new JTextArea();
+	static JFrame window = new JFrame();
+	static JButton knapp = new JButton("Klar");
+	static JTextArea textruta = new JTextArea();
+	static JLabel label = new JLabel("Ange ditt namn:");
+	static String name = new String();
 
-        //
-        // Man lögger komponenterna i en
-        // "content pane" (innehöllspanel).
-        //
-        Container content = window.getContentPane();
+	public static void inputName() {
 
-        //
-        // Hör talar vi om att vi inte vill ha nögon
-        // automatisk layouthantering av komponenterna
-        // i fönstret (null = ingenting/tomt).
-        //
-        content.setLayout(null);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize((int) (Settings.width() * 0.25),
+				(int) (Settings.height() * 0.25)); // Bredd och höjd.
+		window.setLocation((int) (Settings.width() * 0.4),
+				(int) (Settings.height() * 0.4)); // x och y.
+		window.setTitle("HighScore");
+		//
+		// Man lögger komponenterna i en
+		// "content pane" (innehöllspanel).
+		//
+		Container content = window.getContentPane();
 
-        //
-        // Nu lögger vi in komponenterna och positionerar dem.
-        // Koordinatsystemet börjar i övre vönstra hörnet,
-        // y vöxer nedöt. Enheten ör bildpunkter.
-        //
-        content.add(knapp);
-        content.add(textruta);
+		//
+		// Hör talar vi om att vi inte vill ha nögon
+		// automatisk layouthantering av komponenterna
+		// i fönstret (null = ingenting/tomt).
+		//
+		content.setLayout(null);
 
-        //
-        // Argumenten till setBounds() ör x, y, bredd, höjd.
-        //
-        textruta.setBounds(20, 20, 250, 250);
-        knapp.setBounds(20, 290, 250, 40);
-        
-        //
-        // öppna fönstret.
-        //
-        window.show();
-    }
+		//
+		// Nu lögger vi in komponenterna och positionerar dem.
+		// Koordinatsystemet börjar i övre vönstra hörnet,
+		// y vöxer nedöt. Enheten ör bildpunkter.
+		//
+		content.add(knapp);
+		content.add(label);
+		content.add(textruta);
+		knapp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+
+				if (ae.getSource() == knapp) {
+					name = textruta.getText();
+					main.shutDown();
+				}
+			}
+		});
+
+		//
+		// Argumenten till setBounds() ör x, y, bredd, höjd.
+		//
+		label.setBounds((int) (window.getWidth() * 0.015),
+				(int) (window.getHeight() * 0.005),
+				(int) (window.getWidth() * 0.97),
+				(int) (window.getHeight() * 0.20));
+		textruta.setBounds((int) (window.getWidth() * 0.015),
+				(int) (window.getHeight() * 0.20),
+				(int) (window.getWidth() * 0.97),
+				(int) (window.getHeight() * 0.10));
+		knapp.setBounds((int) (window.getWidth() * 0.33),
+				(int) (window.getHeight() * 0.35),
+				(int) (window.getWidth() * 0.33),
+				(int) (window.getHeight() * 0.20));
+
+		//
+		// öppna fönstret.
+		//
+		window.show();
+
+	}
+
+	public static String getName() {
+		return name;
+	}
+
 }
