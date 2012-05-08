fc8dc2953695582db66a63516d312e56cb8ffe68
Robert Krogh <checkpoint2000dx@hotmail.com>
2012-05-08 21:54:15 +0200

    Småfix

--- src/HighScoreMenu.java
+++ src/HighScoreMenu.java
@@ -1,25 +1,28 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 
 public class HighScoreMenu
 {
-    public static void inputName()
+	Settings set = new Settings();
+    public void inputName(int highScore)
     {
         //
         // Skapa ett fönster.
         //
         JFrame window= new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(300, 400);  // Bredd och höjd.
-        window.setLocation(100, 100);  // x och y.
-        window.setTitle("Fönster med komponenter");
+        window.setSize((int)(set.width()*0.2), (int)(set.height()*0.2));  // Bredd och höjd.
+        window.setLocation((int)(set.width()*0.5), (int)(set.height()*0.5));  // x och y.
+        window.setTitle("Input name:");
 
         //
         // Skapa knappen och textrutan.
         //
-        JButton knapp = new JButton("Klicka mej");
-        JTextArea textruta = new JTextArea();
+        JButton button = new JButton("Fatastic");
+        JTextArea textBox = new JTextArea();
+        JLabel label = new JLabel("Your score:" + highScore);
 
         //
         // Man lögger komponenterna i en
@@ -39,14 +42,15 @@ public class HighScoreMenu
         // Koordinatsystemet börjar i övre vönstra hörnet,
         // y vöxer nedöt. Enheten ör bildpunkter.
         //
-        content.add(knapp);
-        content.add(textruta);
+        content.add(button);
+        content.add(textBox);
 
         //
         // Argumenten till setBounds() ör x, y, bredd, höjd.
         //
-        textruta.setBounds(20, 20, 250, 250);
-        knapp.setBounds(20, 290, 250, 40);
+        textBox.setBounds((int)(set.width()*0.5), (int)(set.height()*0.5),(int)(window.WIDTH*0.98),(int)(window.HEIGHT*0.8));
+        button.setBounds(20, 290, 250, 40);
+        
         
         //
         // öppna fönstret.
