2f5d37b233118248df4d68caf80a6ce4d897a912
Jonas Skog <Jonsk197@student.liu.se>
2012-05-10 20:11:08 +0200

    knapp är nu button

--- src/HighScoreMenu.java
+++ src/HighScoreMenu.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 public class HighScoreMenu {
 
 	static JFrame window = new JFrame();
-	static JButton knapp = new JButton("Klar");
+	static JButton button = new JButton("Klar");
 	static JTextArea textruta = new JTextArea();
 	static JLabel label = new JLabel("Ange ditt namn:");
 	static String name = new String();
@@ -37,15 +37,15 @@ public class HighScoreMenu {
 		// Koordinatsystemet börjar i övre vönstra hörnet,
 		// y vöxer nedöt. Enheten ör bildpunkter.
 		//
-		content.add(knapp);
+		content.add(button);
 		content.add(label);
 		content.add(textruta);
-		knapp.addActionListener(new ActionListener() {
+		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
 
-				if (ae.getSource() == knapp) {
+				if (ae.getSource() == button) {
 					name = textruta.getText();
-					main.shutDown();
+					ShowHighScore.showScore();
 				}
 			}
 		});
@@ -61,7 +61,7 @@ public class HighScoreMenu {
 				(int) (window.getHeight() * 0.20),
 				(int) (window.getWidth() * 0.97),
 				(int) (window.getHeight() * 0.10));
-		knapp.setBounds((int) (window.getWidth() * 0.33),
+		button.setBounds((int) (window.getWidth() * 0.33),
 				(int) (window.getHeight() * 0.35),
 				(int) (window.getWidth() * 0.33),
 				(int) (window.getHeight() * 0.20));
